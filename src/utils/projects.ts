56068df36beb30c2901377eedc66a8381e5267cd
Jose Lima <jalimag0206@gmail.com>
2023-09-16 01:31:31 -0300

    feat: adding new projects

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -1,20 +1,37 @@
 export const projects = [
   {
+    title: 'gofinances',
+    description:
+      'Aplicativo de controle financeiro desenvolvido com React native, Typescript e Styled Components.',
+    url: 'https://github.com/devjoselima/gofinances'
+  },
+  {
     title: 'Efood',
     description:
-      'e-commerce de restaurantes desenvolvido com Typescript, React e Styled Components',
+      'e-commerce de restaurantes desenvolvido com Typescript, React e Styled Components.',
     url: 'https://efood-rho.vercel.app/'
   },
   {
     title: 'Eplay',
     description:
-      'e-commerce de games desenvolvido com Typescript, React e Styled Components',
+      'e-commerce de games desenvolvido com Typescript, React e Styled Components.',
     url: 'https://eplay-1-tan.vercel.app/'
   },
   {
-    title: 'gofinances',
+    title: 'ToDoList',
+    description: 'Uma lista de tarefas desenvolvida com VueJS e Bootstrap.',
+    url: 'https://to-do-vue-pearl.vercel.app/'
+  },
+  {
+    title: 'Quiz da copa do mundo',
     description:
-      'Aplicativo de controle financeiro desenvolvido com React native, Typescript e Styled Components',
-    url: 'https://github.com/devjoselima/gofinances'
+      'Um quiz divertido sobre a copa do mundo desenvolvido com Html, Css e Javascript.',
+    url: 'https://quiz-copa.vercel.app/'
+  },
+  {
+    title: 'DevDiary',
+    description:
+      'Um gerenciador de notas feito com Javascript puro com um CRUD simples',
+    url: 'https://dev-diary-zeta.vercel.app '
   }
 ];
